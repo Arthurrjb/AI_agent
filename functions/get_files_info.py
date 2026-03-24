dbc060f0ffe2fc60b3816781dda5f2723b3677df
Arthurrjb <arthurrjb@gmail.com>
2026-03-24 15:57:45 +0100

    reading and writing functions and tests implemented

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,13 +1,22 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    working_dir_abs = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
-    if valid_target_dir == False:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(directory):
-        return f'Error: "{directory}" is not a directory'
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        if valid_target_dir == False:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
 
-testando = get_files_info("calculator", "bunda")
-print(testando, "AAAA")
+        string_list = []
+        for item in os.listdir(target_dir):
+            item_path = os.path.join(target_dir, item)
+            string_list.append(f"- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}")
+            
+        return "\n".join(string_list)
+
+
+    except Exception as e:
+        return f"Error:{e}"
